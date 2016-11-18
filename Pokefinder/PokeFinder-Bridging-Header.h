c47b4d4b88485fb827bbbfda6798ad2b7a70915e
Steve Mecking <stevo@Steves-MacBook-Air.local>
2016-11-18 08:16:14 -0700

    Added GeoFire

---
+++ Pokefinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  Pokefinder
+//
+//  Created by Steve Mecking on 2016-11-18.
+//  Copyright © 2016 Steve Mecking. All rights reserved.
+//
+
+#import "GeoFire.h"
